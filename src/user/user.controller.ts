a4d615c9b6336c528d49c1753bbcfb3bf8eadb4f
Nasim <mdnasimhosen333@gmail.com>
2025-08-30 22:29:06 +0600

    Refactor UserController to utilize UserService for fetching users and add getUserById method

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
    @Get()
     getAllUsers() {
-        return "List of all users"
+        const users  = new UserService().getAllUsers();
+        return users;
+    }
+
+    getUserById(id: number) {
+        const user = new UserService().getUserById(id);
+        return user;
     }
 }
 
