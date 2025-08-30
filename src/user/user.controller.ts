5393e4440a2b44ad88e9672e5ddb3dd188ee9bcb
Nasim <mdnasimhosen333@gmail.com>
2025-08-30 22:23:19 +0600

    Implement getAllUsers method in UserController to return a list of all users

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+   @Get()
+    getAllUsers() {
+        return "List of all users"
+    }
+}
 
 // Get: /user
 // Get: /user/:id
