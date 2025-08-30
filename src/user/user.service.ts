fa7467972760f5c2e3610544bb0e214402e80f20
Nasim <mdnasimhosen333@gmail.com>
2025-08-30 22:25:09 +0600

    Implement UserService with user data and getAllUsers method

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    
+    private users = [
+        { id: 1, name: 'John Doe', type: 'admin' },
+        { id: 2, name: 'Jane Doe', type: 'user' },
+        { id: 3, name: 'Jim Doe', type: 'user' }
+    ];
+
+    getAllUsers() {
+        return this.users;
+    }
+
+
+}
